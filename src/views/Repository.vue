000961c57f4747371ad74d4f78d096b1d701647e
Youngtechie <olaegbeolamilekan1@gmail.com>
2023-03-18 10:15:02 +0100

    :Debugging

--- src/views/Repository.vue
+++ src/views/Repository.vue
@@ -3,16 +3,18 @@
         <div className="co-md-6 d-flex justify-content-center" key={{itm.id}}>
          <div className="card m-2 cbl text center">
            <div className="card-body">
-            <img className='my_avatar' src={{itm.owner.avatar_url}} alt="profile_image" />
+            <img className='my_avatar' :src="itm.owner.avatar_url" alt="profile_image" :width="60" :height="60" />
+            <!--You can change the width and height of this img later-->
              <span className="card-number">ID: {{itm.id}}</span> 
-             <h5 className="card-title mb-4">Repository Name: {itm.full_name}</h5>
-             <p className="card-text">Description: {itm.description}</p>
-             <button className="button-85" >
-               
+             <h5 className="card-title mb-4">Repository Name: {{itm.name}}</h5>
+             <!--The better way to use text interpolation is using {{  }} not one {}-->
+             <p className="card-text">Description: {{itm.description === null ? 'No Description': itm.description}}</p>
+             <!--What I did here was, if description is null 'no description' should be shown or the description if it is present-->
+             
                <a href={{itm.html_url}} className="link-btn" target="_blank" rel="noreferrer">
-               {{itm.name}}
+                <button className="button-85" >View on github</button>
                </a>
-             </button>
+             
            </div>
          </div>
        </div>
@@ -22,7 +24,7 @@
 </template>
 
 <script>
-import { reactive, onMounted } from 'vue';
+import { reactive, watchEffect } from 'vue';
 
 export default {
   setup() {
@@ -32,17 +34,17 @@ export default {
     });
 
     // Define a function to fetch the data from the API
-    async function fetchData() {
+    watchEffect(async () => {
       const response = await fetch('https://api.github.com/users/Orezy01/repos');
-      const jsonData = await response.json();
-      data.items = jsonData;
-      console.log(data.items);
-    }
+      try {
+        const jsonData = await response.json();
+        data.items = jsonData;
+      } catch (error) {
+        console.log(error)  
+      }
+    })
 
     // Call the fetchData function when the component is mounted
-    onMounted(() => {
-      fetchData();
-    });
 
     return {
       data,
