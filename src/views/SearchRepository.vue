65193843903306f121829966f4302572ae371c50
Oreoluwa <orezyjosh@gmail.com>
2023-03-18 12:49:24 +0100

    made changes to the SearchRepository page

--- src/views/SearchRepository.vue
+++ src/views/SearchRepository.vue
@@ -6,12 +6,15 @@
     <div class="input-group mb-3">
   <input type="text" v-model="searchInput" class="form-control" placeholder="Search A Repository" aria-label="Search" aria-describedby="button-addon2">
   <button class="btn btn-outline-secondary" type="button" id="button-addon2" @click="getData(searchInput)">Search</button>
-  <div v-if="isLoading">
+  <div v-if="repoData.length < 0 && isLoading == true">
     Loading Data...
+
   </div>
-  <!--<div v-else v-for="data in fetchData" :key="data.id">
-    {{ data }}
-  </div>-->
+ 
+  <div v-else>
+    {{repoData.created_at}}
+    <h2>{{  }}</h2>
+  </div>
 </div>
 
   </div>
@@ -24,16 +27,23 @@ setup() {
  const fetchData = ref([]);
  const isLoading = ref(true)
  const searchInput = ref('');
+ let id;
+ const repoData = ref([])
 
  const getData = async (name) => {
   if(name !== '' && name !== null){
  try {
+  
   const res = await fetch(`https://api.github.com/users/${name}`)
   fetchData.value = await res.json()
-  console.log(fetchData.value)
-  if(fetchData.value.length > 0){
+  id = setTimeout(() => {
+    repoData.value = fetchData.value;
+    if(repoData.value.length > 0){
   isLoading.value = false
   }
+  console.log(fetchData.value)
+  },3000)
+ 
  } catch (error) {
   console.log(error)
  }
@@ -47,7 +57,9 @@ setup() {
       fetchData ,
       isLoading,
       searchInput,
-      getData
+      getData,
+      id,
+      repoData 
     };
 }
 }
